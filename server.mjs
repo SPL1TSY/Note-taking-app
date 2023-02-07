f67d78bef017c14696a4112c7002473a9f431d47
Harun Hurtic <harunhurtic@gmail.com>
2023-02-07 13:15:48 +0100

    New files for node node_modules

---
+++ server.mjs
@@ -0,0 +1,24 @@
+//#region
+
+import express from 'express'
+
+const server = express();
+const port = (process.env.PORT || 8080);
+server.set('port', port);
+
+//#endregion
+
+server.use(express.static('public'));
+
+server.get("/", (req,res,next) => { //lamda function, arrow function
+
+
+
+res.status(200).send('SHEEESH').end(); //sørg for at send er det siste man gjør?
+res.send ("Forresten, glemte en ting");
+
+});
+
+server.listen(server.get('port'), function () {
+console.log('server running', server.get('port'));
+});
